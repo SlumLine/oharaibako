2b6d916392a72f939ca39b3e56626529c17c6c68
wazaki0713 <109573897+wazaki0713@users.noreply.github.com>
2022-07-19 15:42:32 +0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+$(function(){
+
+  /* 左上 運行情報の時間表示 */
+  setInterval(showtime, 1000);
+
+  function showtime() {
+    var today = new Date();
+    $('#now_time').html(today.getHours() + ":" + ('0' + today.getMinutes()).slice(-2) );
+  }
+  /* 左上 運行情報の時間表示 ここまで */
+
+  /* 文字を左右移動するアニメーション */
+  $(document).ready(function(){
+    $('.moving_text .wrap').on('animationend', function () {
+        if ($(this).attr('data-order') === 'left') {
+            $(this).attr('data-order', 'right');
+        } else {
+            $(this).attr('data-order', 'left');
+        }
+    });
+  });
+  /* 文字を左右移動するアニメーション ここまで */
+
+  /* 右画面切り替え */
+  $("ul#left_nav li").on("click",function(){
+    let show_right_id = $(this).data("link");
+
+    $("ul#left_nav li").each(function(){
+      let right_id = $(this).data("link");
+      if(right_id === show_right_id){
+        $('#'+show_right_id).removeClass("d-none");
+      } else {
+        $('#'+right_id).addClass("d-none");
+      }
+    })
+  })
+  /* お問い合わせ>よくある質問はこちら */
+  $("#right_contact a.link").on("click",function(){
+    $('#right_qa').removeClass("d-none");
+    $('#right_contact').addClass("d-none");
+    return false;
+  })
+})
